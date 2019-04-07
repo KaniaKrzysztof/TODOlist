7201e72f1234c726dcdff3ef8007c99b00f85255
Krzysztof Kania <kontakt@krzysztofkania.com>
2019-04-07 12:11:02 +0200

    Add TaskApi with GET and POST functionality

---
+++ TaskApi/Controllers/TaskController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TaskApi.Controllers
+{
+    [Route("api/task")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private readonly TaskContext _context;
+
+        public TaskController(TaskContext context)
+        {
+            _context = context;
+
+            //if (_context.TaskItems.Count() == 0)
+            //{
+            //    // Create a new TodoItem if collection is empty,
+            //    // which means you can't delete all TodoItems.
+            //    _context.TaskItems.Add(new TaskItem { Content = "Item1" });
+            //    _context.SaveChanges();
+            //}
+        }
+
+        // GET: api/task
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TaskItem>>> GetTaskItems()
+        {
+            return await _context.TaskItems.ToListAsync();
+        }
+
+        // GET: api/task/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TaskItem>> GetTaskItem(long id)
+        {
+            var taskItem = await _context.TaskItems.FindAsync(id);
+
+            if (taskItem == null)
+            {
+                return NotFound();
+            }
+
+            return taskItem;
+        }
+
+        // POST: api/task
+        [HttpPost]
+        public async Task<ActionResult<TaskItem>> PostTaskItem(TaskItem item)
+        {
+            _context.TaskItems.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTaskItem), new { id = item.Id }, item);
+        }
+    }
+}
---
+++ TaskApi/Models/TaskItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskApi.Models
+{
+    public class TaskItem
+    {
+        public long Id { get; set; }
+        public string Content { get; set; }
+        public DateTime DateCreated { get; protected set; }
+        public bool IsComplete { get; set; }
+
+        public TaskItem()
+        {
+            DateCreated = DateTime.Now;
+        }
+    }
+}
